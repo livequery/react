238374dc711c98c30d906743e50247c72ba6e695
Ba <duongvanba.com@gmail.com>
2021-07-01 18:47:23 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { useDocumentData } from "./useDocumentData";
 export { LiveQueryContextProvider } from "./LiveQueryContext";
-export { useAction } from './useAction'
 export { useCollectionData } from './useCollectionData'
-export { useFunctionMonitor } from './useFunctionMonitor'
+export { useMonitor } from './useMonitor'
---
+++ src/useMonitor.ts
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+type PromiseType<T> = T extends PromiseLike<infer U> ? U : T
+
+export const useMonitor = <T extends (...args: any) => any>(fn: T) => {
+    const [{ error, data, loading }, update] = useState<{ error?: any, data?: PromiseType<ReturnType<T>>, loading?: boolean }>({})
+
+    const excute = (async (...args: any[]) => {
+        update({ data: null, loading: true, error: null })
+        try {
+            update({ data: await fn(...args), error: null, loading: false })
+        } catch (error) {
+            update({ data: null, error, loading: false })
+        }
+    }) as any as T
+
+    const vaild = !error && !loading
+
+    return { excute, loading, data, error, vaild }
+}
