9a50dbbaa190635765b4c712e7a3ac9c63c64239
Ba <duongvanba.com@gmail.com>
2021-07-15 09:21:40 +0700

    Update

--- src/hooks/createContextFromHook.tsx
+++ src/hooks/createContextFromHook.tsx
@@ -4,10 +4,10 @@ import { createContext, useContext } from "react"
 export const createContextFromHook = <T, K>(fn: (props?: T) => K) => {
     const context = createContext<K>({} as K)
     const getContext = () => useContext(context)
-    const Provider = ({ children, ...value }: PropsWithChildren<K>) => {
-        <context.Provider value={value as K}>
+    const Provider = ({ children, ...value }: PropsWithChildren<K>) => (
+        <context.Provider value={value as K ?? fn()}>
             {children}
         </context.Provider>
-    }
+    )
     return [fn, getContext, Provider] as [typeof fn, typeof getContext, typeof Provider]
 }
