34cb99df75609c1436f3b500af8a10d23bca60b9
Ba <BA>
2026-04-28 17:13:21 +0700

    Update

--- src/useDocument.ts
+++ src/useDocument.ts
@@ -1,11 +1,11 @@
-import { LivequeryDocument, type Doc, type DocState } from "@livequery/core"
+import { type Doc } from "@livequery/core"
 import { useObservable } from "./useObservable.js"
 import { useCollection } from "./useCollection.js"
 
 
 export const useDocument = <T extends Doc>(ref: string | undefined | '' | null | false, options: { lazy?: boolean } = {}) => {
     const collection = useCollection<T>(ref, { lazy: options.lazy })
-    const items = useObservable(collection.items, [] as LivequeryDocument<DocState<T>>[])
+    const items = useObservable(collection.items)
     const loading = useObservable(collection.loading)
     return [items[0], loading] as const
 }
--- src/useObservable.ts
+++ src/useObservable.ts
@@ -1,25 +1,34 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Observable, BehaviorSubject, tap, EMPTY } from "rxjs";
 import { skip } from "rxjs/operators";
 
 export type MaybeFunction<T> = T | (() => T)
 
-export const useObservable = <T extends any, R = undefined>(o: MaybeFunction<BehaviorSubject<T> | Observable<T>> | undefined, default_value?: R) => {
-    const getValue = (o as any)?.getValue
-    const [v, s] = useState<T | R>(getValue ? getValue() as T : default_value as R)
+type ObservableSource<T> = MaybeFunction<BehaviorSubject<T> | Observable<T>> | undefined
+
+export function useObservable<T>(o: BehaviorSubject<T>): T
+export function useObservable<T>(o: ObservableSource<T>): T | undefined
+export function useObservable<T>(o: ObservableSource<T>, default_value: T): T
+
+export function useObservable<T>(o: MaybeFunction<T>, default_value?: T) {
+    const prev = useRef(o)
+    const $ = o as any as BehaviorSubject<T> || EMPTY
+    const isBehaviorSubject =typeof o == 'object' &&  typeof $.getValue === 'function'
+    const dfv = isBehaviorSubject ? $.getValue() : default_value
+    const [v, s] = useState<T | undefined>(dfv)
     useEffect(() => {
+        const diff = prev.current !== o
+        prev.current = o
         try {
-            const target = (typeof o === 'function' ? o() : (o || EMPTY))
-            const subscription = target.pipe(
-                skip(getValue ? 1 : 0),
-                tap(value => {
-                    s(value)
-                })
+            const subscription = $.pipe(
+                skip(isBehaviorSubject && !diff ? 1 : 0),
+                tap(s)
             ).subscribe()
             return () => {
                 subscription.unsubscribe()
             }
         } catch (e) { }
     }, typeof o === 'function' ? [] : [o])
+
     return v
 }
