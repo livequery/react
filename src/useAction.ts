e4e22a500a332450e26dc9a7dd3b55ec5a156126
Ba <BA>
2026-05-15 14:00:28 +0700

    release: bump version to 2.0.112

--- src/useAction.ts
+++ src/useAction.ts
@@ -1,21 +1,22 @@
 import { useState } from "react"
 
 export const useAction = <T extends (...args: any[]) => Promise<any>>(fn: T, options: Partial<{ onError: Function }> = {}) => {
-    const [state, set_state] = useState({
-        loading: false,
-        data: null,
-        error: null as null | { code: string, message: string }
-    })
+    const [state, set_state] = useState<{
+        loading: boolean,
+        data?: Awaited<ReturnType<T>>
+        error?: { code: string, message: string }
+    }>({ loading: false })
+
     const f = async (...args: any) => {
-        set_state({ loading: true, data: null, error: null })
+        set_state({ loading: true })
         try {
             const data = await fn(...args)
-            set_state({ loading: false, data, error: null })
+            set_state({ loading: false, data })
             return data
         } catch (error) {
             options.onError?.(error)
-            set_state({ loading: false, data: null, error: error instanceof Error ? { code: 'error', message: error.message } : { code: 'error', message: String(error) } })
+            set_state({ loading: false, error: error instanceof Error ? { code: 'error', message: error.message } : { code: 'error', message: String(error) } })
         }
     }
     return Object.assign(f as T, state)
-}
+} 
